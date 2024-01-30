e72bcc86691fc806255c842de81636dafbdcda11
jarvan.z <jarvan.z@outlook.com>
2024-01-30 17:05:37 +0800

    add google analytics

---
+++ src/initGa.tsx
@@ -0,0 +1,8 @@
+// Copyright 2023-2023 dev.mimir authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import ReactGA from 'react-ga4';
+
+export function initGa() {
+  ReactGA.initialize('G-N1N38RJE6Q');
+}
