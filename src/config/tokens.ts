d248a571bca042ab1c60fdfbd6ae2f50e75551d3
jarvan.z <jarvan.z@outlook.com>
2024-04-10 22:28:32 +0800

    feat: Add dota asset token

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -28,6 +28,11 @@ export const assets: Asset[] = [
     Icon: '/token-icons/DED.webp',
     genesisHash: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f',
     assetId: '30'
+  },
+  {
+    Icon: '/token-icons/DOTA.png',
+    genesisHash: '0x68d56f15f85d3136970ec16946040bc1752654e906147f7e43e9d539d7c3de2f',
+    assetId: '18'
   }
 ];
 
