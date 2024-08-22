270e8ed167ee564e3529584c57c527a43aa75638
jarvan.z <jarvan.z@outlook.com>
2024-08-22 10:32:32 +0800

    feat: Add some blockchains

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,7 @@
 // Copyright 2023-2024 dev.mimir authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+export * from './api-types';
 export * from './api';
 export * from './dapp';
 export * from './utm';
