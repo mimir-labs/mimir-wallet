bbb7eebff1f3735533e4a5e06f6ef6c86b0d0c1d
jarvandev <131938987+jarvandev@users.noreply.github.com>
2024-12-24 13:58:58 +0800

    feat: optimize connect wallet layout (#97)

--- src/components/ConnectWalletModal.tsx
+++ src/components/ConnectWalletModal.tsx
@@ -2,6 +2,8 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import {
+  alpha,
+  Box,
   Button,
   Dialog,
   DialogContent,
@@ -13,7 +15,7 @@ import {
   useTheme
 } from '@mui/material';
 import { AccessCredentials, initializePlutonicationDAppClientWithModal } from '@plutonication/plutonication';
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { walletConfig } from '@mimir-wallet/config';
 import { useWallet } from '@mimir-wallet/hooks';
@@ -28,6 +30,68 @@ function WalletCell({ downloadUrl, id, name }: { name: string; id: string; downl
 
   const isInstalled =
     id === 'nova' ? wallets[walletConfig[id].key] && window?.walletExtension?.isNovaWallet : wallets[id];
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        gap: 0.5,
+        borderRadius: 1,
+        border: '1px solid',
+        borderColor: 'secondary.main',
+        padding: 1,
+        bgcolor: connectedWallets.includes(id) ? 'secondary.main' : 'transparent'
+      }}
+    >
+      <WalletIcon disabled={!isInstalled} id={id} sx={{ width: 20, height: 20 }} />
+      <Box sx={{ flex: 1, fontSize: '1rem' }}>{name}</Box>
+      {isInstalled ? (
+        connectedWallets.includes(id) ? (
+          <Button
+            color='error'
+            onClick={() => disconnect(id)}
+            size='small'
+            variant='text'
+            sx={({ palette }) => ({ bgcolor: alpha(palette.error.main, 0.1) })}
+          >
+            Disconnect
+          </Button>
+        ) : (
+          <Button onClick={() => connect(id).catch(toastError)} size='small' variant='outlined'>
+            Connect
+          </Button>
+        )
+      ) : id === 'plutonication' ? (
+        <Button
+          onClick={async () => {
+            const accessCredentials = new AccessCredentials(
+              'wss://plutonication.com/',
+              'Mimir',
+              'https://plutonication.com/dapp/mimir-icon',
+              'Mimir'
+            );
+
+            await initializePlutonicationDAppClientWithModal(accessCredentials, (receivedPubkey: string) => {
+              /* */
+              console.log(receivedPubkey);
+            });
+
+            connect('plutonication').catch(toastError);
+          }}
+          size='small'
+          variant='outlined'
+        >
+          Connect
+        </Button>
+      ) : (
+        <Button component='a' href={downloadUrl} size='small' target='_blank' variant='text'>
+          Download
+        </Button>
+      )}
+    </Box>
+  );
 
   return (
     <Stack
@@ -80,14 +144,31 @@ function WalletCell({ downloadUrl, id, name }: { name: string; id: string; downl
 }
 
 function ConnectWalletModal({ onClose, open }: { open: boolean; onClose: () => void }) {
+  const { connectedWallets, wallets } = useWallet();
+
+  // Sort walletConfig: connected first, unconnected second, not installed last
+  const sortedWalletConfig = useMemo(() => {
+    return Object.entries(walletConfig).sort(([id], [id2]) => {
+      const isInstalled =
+        id === 'nova' ? wallets[walletConfig[id].key] && window?.walletExtension?.isNovaWallet : wallets[id];
+      const isInstalled2 =
+        id2 === 'nova' ? wallets[walletConfig[id2].key] && window?.walletExtension?.isNovaWallet : wallets[id2];
+
+      const left = connectedWallets.includes(id) ? 2 : isInstalled ? 1 : isInstalled2 ? 0 : -1;
+      const right = connectedWallets.includes(id2) ? 2 : isInstalled2 ? 1 : isInstalled ? 0 : -1;
+
+      return right - left;
+    });
+  }, [wallets, connectedWallets]);
+
   return (
     <>
-      <Dialog maxWidth='md' onClose={onClose} open={open}>
+      <Dialog maxWidth='sm' onClose={onClose} open={open}>
         <DialogTitle textAlign='center'>Connect Wallet</DialogTitle>
-        <DialogContent sx={{ overflow: 'hidden' }}>
-          <Grid columns={{ xs: 12 }} container spacing={{ sm: 4, xs: 2 }}>
-            {Object.entries(walletConfig).map(([id, config]) => (
-              <Grid key={id} size={{ sm: 3, xs: 4 }}>
+        <DialogContent sx={{ overflow: 'hidden', maxWidth: '474px' }}>
+          <Grid columns={{ sm: 4, xs: 2 }} container rowSpacing={2} columnSpacing={1}>
+            {sortedWalletConfig.map(([id, config]) => (
+              <Grid key={id} size={2}>
                 <WalletCell downloadUrl={config.downloadUrl} id={id} name={config.name} />
               </Grid>
             ))}
