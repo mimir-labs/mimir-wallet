d94d8c258c85954cba0c6487128a8cf2b2fd3872
jarvan.z <jarvan.z@outlook.com>
2025-03-26 14:02:53 +0800

    fix: fix create flexible multisig error when creating multisig account

--- app/src/hooks/useCacheMultisig.ts
+++ app/src/hooks/useCacheMultisig.ts
@@ -5,6 +5,7 @@ import type { CacheMultisig } from './types';
 
 import { service } from '@/utils';
 
+import { addressToHex } from '@mimir-wallet/polkadot-core';
 import { useQuery } from '@mimir-wallet/service';
 
 import { useAddressStore } from './useAddressStore';
@@ -13,10 +14,14 @@ export function useCacheMultisig(): [data: CacheMultisig[], isLoading: boolean] 
   const { accounts } = useAddressStore();
   const { data, isLoading } = useQuery<CacheMultisig[]>({
     refetchInterval: false,
-    queryHash: service.getNetworkUrl(`multisig/pending?${accounts.map((address) => `addresses=${address}`).join('&')}`),
+    queryHash: service.getNetworkUrl(
+      `multisig/pending?${accounts.map(({ address }) => `addresses=${addressToHex(address)}`).join('&')}`
+    ),
     queryKey: [
       accounts.length > 0
-        ? service.getNetworkUrl(`multisig/pending?${accounts.map((address) => `addresses=${address}`).join('&')}`)
+        ? service.getNetworkUrl(
+            `multisig/pending?${accounts.map(({ address }) => `addresses=${addressToHex(address)}`).join('&')}`
+          )
         : null
     ]
   });
